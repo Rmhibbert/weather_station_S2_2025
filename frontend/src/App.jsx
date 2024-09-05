51e00465e9da9633acd9b2c19808534b12cf9235
Benjamin Campbell-Nelson (1000069351) <CAMPBF2@student.op.ac.nz>
2024-09-05 15:00:28 +1200

    chore(jsx): add coming soon messaging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,16 @@ function App() {
   const [airPressure] = useState('1013 hPa');
   const [humidity] = useState('123');
   const [wind] = useState('fast');
+  const [data] = useState('coming soon');
 
   return (
     <div className="app-container">
       <Header />
       <div className="widgets">
-      <Widget name={'Temperature'} data={temperature}/>
-      <Widget name={'AirPressure'} data={airPressure}/>
-      <Widget name={'Humidity'} data={humidity}/>
-      <Widget name={'Wind'} data={wind}/>
+      <Widget name={'Temperature'} data={data}/>
+      <Widget name={'AirPressure'} data={data}/>
+      <Widget name={'Humidity'} data={data}/>
+      <Widget name={'Wind'} data={data}/>
       <Webcam />
     
       </div>
