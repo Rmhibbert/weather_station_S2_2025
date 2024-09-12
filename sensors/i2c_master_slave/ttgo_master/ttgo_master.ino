1406471bd248eec72edcb90f0296ef9026a563fb
Kieren <dunnersguy@gmail.com>
2024-09-12 14:44:46 +1200

    added ability to do wind and rain in seperate payload

--- sensors/i2c_master_slave/ttgo_master/ttgo_master.ino
+++ sensors/i2c_master_slave/ttgo_master/ttgo_master.ino
@@ -21,11 +21,14 @@ void os_getDevKey(u1_t* buf) {
   memcpy_P(buf, APPKEY, 16);
 }
 
-unsigned char payload[7];
+unsigned char payload[5];
 unsigned char payloadR[5];
 static osjob_t sendjob;
 
-const unsigned TX_INTERVAL = 60;
+const unsigned TX_INTERVAL = 10;
+
+// State variable to track which payload to send
+int payloadState = 0;  // 0 = temperature/pressure, 1 = temperature/CO2
 
 // Pin mapping for Dragino Shield
 const lmic_pinmap lmic_pins = {
@@ -40,10 +43,10 @@ void setup() {
   Serial.begin(9600);
   Serial.println(F("Starting"));
 
-  Wire.requestFrom(0x08, 5);  // Request 2 bytes from the slave
-  Wire.readBytes(payloadR, 5);
+  Wire.requestFrom(0x08, 9);  // Request 2 bytes from the slave
+  Wire.readBytes(payloadR, 9);
   Serial.print("Received Data: ");
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 9; i++) {
     Serial.print(payloadR[i], HEX);
     Serial.print(" ");
   }
@@ -51,13 +54,13 @@ void setup() {
 
   // Decode the first two bytes as temperature
   float temperature = decodeTemperature(payloadR[0], payloadR[1]);
-  
+
   // Decode the next two bytes as CO2
   float CO2 = decodeCO2(payloadR[2], payloadR[3]);
 
   Serial.print("Decoded Temperature: ");
   Serial.println(temperature);
-  
+
   Serial.print("Decoded CO2: ");
   Serial.println(CO2);
 
@@ -76,84 +79,95 @@ void onEvent(ev_t ev) {
   Serial.print(": ");
   switch (ev) {
     case EV_JOINED:
-      //Serial.println(F("EV_JOINED"));
       LMIC_setLinkCheckMode(0);
       break;
+
     case EV_TXCOMPLETE:
-      //Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
       if (LMIC.txrxFlags & TXRX_ACK)
-        //Serial.println(F("Received ack"));
-        if (LMIC.dataLen) {
-          //Serial.print(F("Received "));
-          //Serial.print(LMIC.dataLen);
-          //Serial.println(F(" bytes of payload"));
-        }
+        Serial.println(F("Received ack"));
+
+      // Transmission is complete, now switch to the other payload
+      if (payloadState == 0) {
+        payloadState = 1;  // Switch to temperature/CO2
+      } else if (payloadState == 1) {
+        payloadState = 2;  // Switch to temperature/pressure
+      } else if (payloadState == 2) {
+        payloadState = 0;
+      }
+
+      // Schedule the next transmission
       os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
       break;
+
     default:
-      //Serial.print(F("Unknown event: "));
-      //Serial.println((unsigned) ev);
       break;
   }
 }
+
 void do_send(osjob_t* j) {
   // Check if there is not a current TX/RX job running
   if (LMIC.opmode & OP_TXRXPEND) {
-    //Serial.println(F("OP_TXRXPEND, not sending"));
+    Serial.println(F("OP_TXRXPEND, not sending"));
   } else {
-    Wire.requestFrom(0x08, 5);  // Request 2 bytes from the slave
-    Wire.readBytes(payloadR, 5);
+    // Read sensor data (same as in your current code)
+    Wire.requestFrom(0x08, 9);
+    Wire.readBytes(payloadR, 9);
     Serial.print("Received Data: ");
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 9; i++) {
       Serial.print(payloadR[i], HEX);
       Serial.print(" ");
     }
     Serial.println();
 
-    // Decode the first two bytes as temperature
-  float temperatureR = decodeTemperature(payloadR[0], payloadR[1]);
-  
-  // Decode the next two bytes as CO2
-  float CO2R = decodeCO2(payloadR[2], payloadR[3]);
+    // Decide which payload to send based on the payloadState
+    if (payloadState == 0) {
+      // Encode and send temperature and pressure
+      float temperaturePres = get_temp_c();
+      float pressure = get_pressure();
 
-    // Read sensor data
-    float temperature = get_temp_c();
-    float pressure = get_pressure();
+      uint16_t payloadTempPress = encodeFixedPoint100(temperaturePres);
+      uint16_t payloadPressure = encodeFixedPoint1(pressure);
 
-    if (CO2R != 0) temperature = (temperature + temperatureR) / 2;
+      payload[0] = lowByte(payloadTempPress);
+      payload[1] = highByte(payloadTempPress);
+      payload[2] = lowByte(payloadPressure);
+      payload[3] = highByte(payloadPressure);
 
-    Serial.print("Temp: ");
-    Serial.println(temperature);
-    Serial.print("Press: ");
-    Serial.println(pressure);
-    Serial.print("CO2: ");
-    Serial.println(CO2R);
+      LMIC_setTxData2(1, payload, 4, 0);  // Send on port 1
+      Serial.println(F("Sending temperature/pressure"));
 
-    uint16_t payloadTemp = encodeFixedPoint100(temperature);
-    uint16_t payloadPressure = encodeFixedPoint1(pressure);
-    uint16_t payloadCO2 = encodeFixedPoint1(CO2R);
+    } else if (payloadState == 1) {
+      // Encode and send temperature and CO2
+      float temperatureR = decodeTemperature(payloadR[0], payloadR[1]);
+      float CO2R = decodeCO2(payloadR[2], payloadR[3]);
 
-    // Convert to bytes
-    byte tempLow = lowByte(payloadTemp);
-    byte tempHigh = highByte(payloadTemp);
-    byte PressureLow = lowByte(payloadPressure);
-    byte PressureHigh = highByte(payloadPressure);
-    byte CO2Low = lowByte(payloadCO2);
-    byte CO2High = highByte(payloadCO2);
+      uint16_t payloadTempCO2 = encodeFixedPoint100(temperatureR);
+      uint16_t payloadCO2 = encodeFixedPoint1(CO2R);
 
-    // Fill the payload
-    payload[0] = tempLow;
-    payload[1] = tempHigh;
-    payload[2] = PressureLow;
-    payload[3] = PressureHigh;
-    payload[4] = CO2Low;
-    payload[5] = CO2High;
+      payload[0] = lowByte(payloadTempCO2);
+      payload[1] = highByte(payloadTempCO2);
+      payload[2] = lowByte(payloadCO2);
+      payload[3] = highByte(payloadCO2);
 
-    // Prepare upstream data transmission at the next possible time
-    LMIC_setTxData2(1, payload, sizeof(payload) - 1, 0);
-    //Serial.println(F("Packet queued"));
+      LMIC_setTxData2(2, payload, 4, 0);  // Send on port 3
+      Serial.println(F("Sending temperature/CO2"));
+    } else if (payloadState == 2) {
+      // Encode and send wind and rain
+      float Wind = decodeTemperature(payloadR[4], payloadR[5]);
+      float Rain = decodeCO2(payloadR[6], payloadR[7]);
+
+      uint16_t payloadWind = encodeFixedPoint100(Wind);
+      uint16_t payloadRain = encodeFixedPoint1(Rain);
+
+      payload[0] = lowByte(payloadWind);
+      payload[1] = highByte(payloadWind);
+      payload[2] = lowByte(payloadRain);
+      payload[3] = highByte(payloadRain);
+
+      LMIC_setTxData2(3, payload, 4, 0);  // Send on port 2
+      Serial.println(F("Sending temperature/CO2"));
+    }
   }
-  // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void loop() {
