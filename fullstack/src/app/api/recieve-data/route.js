19e26be7887c84ddad81fcd8a6a842c8953e77d0
Jackson Williamson (1000107223) <WILLJM12@student.op.ac.nz>
2024-09-12 15:25:17 +1200

    Added Post request for dust

--- fullstack/src/app/api/recieve-data/route.js
+++ fullstack/src/app/api/recieve-data/route.js
@@ -1,28 +1,32 @@
+import { DustData } from "@/app/utils/recieve-data-helper";
 import db from "@/db";
 
 export const POST = async (request) => {
     try {
         const data = await request.json();
-
-        const device_id = data.end_device_ids.device_id;
+        let send = []
+        const device_id = data.uplink_message.f_port;
         const humidity = data.uplink_message.decoded_payload.humidity;
         const temperature = data.uplink_message.decoded_payload.temperature;
         const pressure = data.uplink_message.decoded_payload.pressure;
         const altitude = data.uplink_message.decoded_payload.altitude;
+        const dust = data.uplink_message.decoded_payload.dustDensity;
 
-        console.log(device_id, pressure, altitude, temperature);
+        if (!device_id){
+            return new Response(JSON.stringify({ message: 'Device ID is required' }), {
+                headers: { 'Content-Type': 'application/json' },
+                status: 400
+            });
+        }
 
-        const send = await db.one(
-            'INSERT INTO sensor_pressure (device_id, pressure) VALUES ($1, $2) RETURNING *',
-            [device_id, pressure]
-        )
-
+        if (dust){
+            await send.push(DustData(device_id, dust));
+        }
 
         return new Response(JSON.stringify(send), {
             status: 200
         });
     } catch (err) {
-        console.error(err);
         return new Response(JSON.stringify({ message: 'Server Error' }), {
             headers: { 'Content-Type': 'application/json' },
             status: 500
