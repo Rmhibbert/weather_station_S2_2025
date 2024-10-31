1edc585ec9a8b9234f8a997f2d888ea9441b4659
BenCampbellNelson <campbellnelsonb@gmail.com>
2024-11-01 12:04:12 +1300

    feat(jsx): create footer

--- fullstack/src/components/footer.jsx
+++ fullstack/src/components/footer.jsx
@@ -1,8 +1,52 @@
 export default function Footer() {
-    return (
-      <footer>
-        <p>Placeholder Footer</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer
+      className="page-footer"
+      style={{
+        padding: "1.5rem 2rem",
+        textAlign: "center",
+        fontSize: "0.85rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: '100%'
+      }}
+    >
+      <div
+        style={{
+          width: "80%",
+          height: "1px",
+          backgroundColor: "rgba(240, 240, 240, 0.7)",
+        }}
+      ></div>
+      <p style={{ margin: "0.5rem 0", lineHeight: 1.5 }}>
+        Weather station is located on the roof of Otago Polytechnic D-Block, 60
+        Harbour Terrace
+      </p>
+      <p style={{ margin: 0, fontWeight: "lighter" }}>
+        Developed by Sam, Kieren, Jackson, Tom and Ben
+      </p>
+      <p style={{ margin: 0, fontWeight: "lighter" }}>
+        Check out our github page:
+      </p>
+      <div style={{ marginTop: "0.5rem" }}>
+        <a
+          href="https://github.com/OtagoPolytechnic/Cloudy-with-a-Chance-of-LoRa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/github-mark-white.png"
+            alt="GitHub Logo"
+            style={{
+              width: "24px",
+              height: "24px",
+              display: "inline-block",
+              verticalAlign: "middle",
+            }}
+          />
+        </a>
+      </div>
+    </footer>
+  );
+}
