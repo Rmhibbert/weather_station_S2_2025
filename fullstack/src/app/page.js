a1d98ea18ca45f3d6bd31ab634367e51b55ef076
BenCampbellNelson <campbellnelsonb@gmail.com>
2024-11-05 10:13:11 +1300

    chore(jsx): LineChart on page for testing

--- fullstack/src/app/page.js
+++ fullstack/src/app/page.js
@@ -5,6 +5,7 @@ import BarChartComponent from '@/components/graphs/BarChartComponent';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import './page.css';
+import { Line } from 'recharts';
 
 export default function Home() {
   return (
@@ -13,13 +14,13 @@ export default function Home() {
 
       {/* Widgets Section */}
       <div className="widgets">
-        <Widget name="Temperature" dataKey="temperature" GraphComponent={null} />
-        <Widget name= "Rain" dataKey="rain" GraphComponent={null}/>
-        <Widget name="Air Pressure" dataKey="pressure" GraphComponent={null} />
-        <Widget name="Wind" dataKey="wind" GraphComponent={null} />
-        <Widget name="CO2" dataKey="co2" GraphComponent={null} />
-        <Widget name="Gas" dataKey="gas" GraphComponent={null} />
-        <Widget name="Dust" dataKey="dust" GraphComponent={null} />
+        <Widget name="Temperature" dataKey="temperature" GraphComponent={LineChartComponent} />
+        <Widget name= "Rain" dataKey="rain" GraphComponent={LineChartComponent}/>
+        <Widget name="Air Pressure" dataKey="pressure" GraphComponent={LineChartComponent} />
+        <Widget name="Wind" dataKey="wind" GraphComponent={LineChartComponent} />
+        <Widget name="CO2" dataKey="co2" GraphComponent={LineChartComponent} />
+        <Widget name="Gas" dataKey="gas" GraphComponent={LineChartComponent} />
+        <Widget name="Dust" dataKey="dust" GraphComponent={LineChartComponent} />
       </div>
 
       <Footer />
