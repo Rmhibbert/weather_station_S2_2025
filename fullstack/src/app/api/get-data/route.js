42e3cd4475a01f773409f902c3e1f46d94461abb
Tom <83677440+TomMc608@users.noreply.github.com>
2024-11-05 16:23:03 +1300

    Update route.js

--- fullstack/src/app/api/get-data/route.js
+++ fullstack/src/app/api/get-data/route.js
@@ -15,7 +15,7 @@ export const GET = async (request) => {
         const authHeader = request.headers.get('authorization');
         const { searchParams } = new URL(request.url);
         const ip = request.headers.get('x-forwarded-for') || request.connection.remoteAddress;
-        const MAX_REQUESTS = 50
+        const MAX_REQUESTS = 10
         const type = searchParams.get('type');
 
         if (!authHeader) return new Response("Authentication Required")
