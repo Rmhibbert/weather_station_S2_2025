c7a07c8c068b3f939661eb1285dfaebbeb649a09
Tom <83677440+TomMc608@users.noreply.github.com>
2024-11-05 17:02:58 +1300

    added a new widget and it links to the api for cloud AI model

--- fullstack/src/app/page.js
+++ fullstack/src/app/page.js
@@ -2,6 +2,7 @@
 import Widget from "@/components/widget";
 import LineChartComponent from "@/components/graphs/LineChartComponent";
 import BarChartComponent from "@/components/graphs/BarChartComponent";
+import Link from 'next/link';  // Import the Link component for internal routing
 import "./page.css";
 
 export default function Home() {
@@ -14,6 +15,11 @@ export default function Home() {
         <Widget name="CO2" dataKey="co2" GraphComponent={null} />
         <Widget name="Gas" dataKey="gas" GraphComponent={null} />
         <Widget name="Dust" dataKey="dust" GraphComponent={null} />
+
+        {/* Link to external site */}
+        <a href="https://huggingface.co/spaces/TomMc9010/Cloud_AI_model" target="_blank" rel="noopener noreferrer">
+          <Widget name="Cloud" GraphComponent={null} />
+        </a>
       </div>
     </div>
   );
