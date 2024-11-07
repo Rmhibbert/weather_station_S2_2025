795d808c5c7721390b8c130e4acb61756aee1c23
Samantha Craig (1000048108) <CRAISR1@student.op.ac.nz>
2024-11-08 09:07:10 +1300

    Updated Cypress config

---
+++ fullstack/__tests__/unit/route.test.jsx
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Dust from '../src/app/api/dust-data/route'
+ 
+test('Dust-data renders correctly.', async () => {
+  render(<Dust />)
+  await waitFor(() => screen.getByText('dust'))
+  const dustMessage = screen.getByText('dust:')
+  expect(dustMessage).toBeInTheDocument()
+})
