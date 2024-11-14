930ac0e462aae24f16a2295eeaaecc3e7db17108
Samantha Craig (1000048108) <CRAISR1@student.op.ac.nz>
2024-11-15 09:03:55 +1300

    Idk I wrote tests

--- fullstack/__tests__/unit/page.test.jsx
+++ fullstack/__tests__/unit/page.test.jsx
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Home from '../../src/app/page'
- 
+
 test('Home', () => {
   render(<Home />)
   expect(screen.getByText('Temperature')).toBeDefined()
