1d84adb89cbf53b34dfe2119f3aa9494918bc1d8
Denise Caponpon (1000108395) <CAPOD2@student.op.ac.nz>
2025-08-11 15:05:27 +1200

    added an about page with header and paragraph

---
+++ fullstack/src/app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <main>
+        <h1>About Us</h1>
+        <p>Welcome to our website! Here’s a little about us...</p>
+      </main>
+    );
+  }
+  
