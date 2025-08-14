24bca0f9be5b67e8d93a1642fea705717b7301ab
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2025-08-14 13:53:50 +1200

    changed fonts adn fix stacked links, added boarder to bottom, got logo to work

--- fullstack/src/components/header.jsx
+++ fullstack/src/components/header.jsx
@@ -1,17 +1,23 @@
-import './header.css'; 
+import './header.css';
 
 export default function Header() {
   return (
     <section className="header">
       <nav>
-        <a href="home"><img src="./Images/logo1.png" alt="Logo" /></a>
+        <a href="home">
+          <img src="/images/logo1.png" alt="Logo" />
+        </a>
         <div className="textbox">
-        <h1>Otago Polytechnic Weather Station</h1>
-      </div>
+          <h1>Otago Polytechnic Weather Station</h1>
+        </div>
         <div className="links">
           <ul>
-            <li><a href="about">ABOUT</a></li>
-            <li><a href="courses">OTAGO POLYTECHNIC</a></li>
+            <li>
+              <a href="about">ABOUT</a>
+            </li>
+            <li>
+              <a href="courses">OTAGO POLYTECHNIC</a>
+            </li>
           </ul>
         </div>
       </nav>
