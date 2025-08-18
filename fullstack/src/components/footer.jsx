a129b93df9cd3f9c0890e26ff56d91f58851cebe
Robebj2 <53206492+Robebj2@users.noreply.github.com>
2025-08-18 14:51:43 +1200

    feat: enlarge logos and put them on each side of footer

--- fullstack/src/components/footer.jsx
+++ fullstack/src/components/footer.jsx
@@ -1,18 +1,33 @@
 import Image from 'next/image';
 export default function Footer() {
-  return (
-    <footer className="page-footer p-6 text-center text-sm flex flex-col items-center w-full">
-      <div className="w-4/5 h-px bg-white"></div>
+  return (    
+    <footer className="page-footer p-6 text-center text-sm w-full">
+      <div className="w-4/5 h-px bg-white mx-auto"></div>
+
+      <div className="text-center mt-2">
       <p className="mt-2 mb-1 leading-relaxed">
         Weather station is located on the roof of Otago Polytechnic D-Block, 60
         Harbour Terrace
-      </p>
+      </p>     
+      
+      </div>      
+      <div className="flex justify-between items-center mt-4 w-full px-8">        
+      <a
+          href="https://www.op.ac.nz/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/images/OP-logo.png"
+            alt="Otago Polytechnic Logo"            
+            width={60}
+            height={60}
+          />
+        </a>
+        
       <p className="m-0 font-light">
-        {/* Developed by Samantha, Kieren, Jackson, Tom, and Ben */}
         Developed by Hayden, Rory, Sophie, Tylor, Denise and Bradley
       </p>
-      <p className="m-0 font-light">Check out our GitHub page:</p>
-      <div className="mt-2">
         <a
           href="https://github.com/Rmhibbert/weather_station_S2_2025"
           target="_blank"
@@ -20,31 +35,12 @@ export default function Footer() {
         >
           <Image
             src="/images/github-mark-white.png"
-            alt="GitHub Logo"
-            className="w-6 h-6 inline-block align-middle"
-            width={24}
-            height={24}
+            alt="GitHub Logo"            
+            width={30}
+            height={30}
           />
         </a>
-      </div>
-
-      
-      <p className="m-0 font-light">Otago Polytechnic Website:</p>
-      <div className="mt-2">
-        <a
-          href="https://www.op.ac.nz/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/images/OP-logo.png"
-            alt="Otago Polytechnic Logo"
-            className="w-6 h-6 inline-block align-middle"
-            width={24}
-            height={24}
-          />
-        </a>
-      </div>
+      </div> 
     </footer>
   );
 }
