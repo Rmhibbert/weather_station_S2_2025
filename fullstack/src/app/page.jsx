53ad8ae1d1ac26ad0fe5f2127575d22d1fce6094
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2025-08-25 13:34:23 +1200

    Fixed main page layout error.

--- fullstack/src/app/page.jsx
+++ fullstack/src/app/page.jsx
@@ -1,33 +1,61 @@
 'use client';
 
+
+
 import Widget from '@/components/widget';
+
 import Header from '../components/header';
+
 import Footer from '../components/footer';
+
 import './page.css';
+
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
 export const queryClient = new QueryClient();
 
+
+
 export default function Home() {
+
   return (
+
     <QueryClientProvider client={queryClient}>
+
       <div className="app-container">
+
         <Header />
 
+
+
         {/* Widgets Section */}
+
+        <div className="widgets">
+
           <Widget name="Temperature" dataKey="temperature" />
+
           <Widget name="Rain" dataKey="rain" />
+
           <Widget name="Air Pressure" dataKey="pressure" />
+
           <Widget name="Wind" dataKey="wind" />
+
           <Widget name="CO2" dataKey="co2" />
+
           <Widget name="Gas" dataKey="gas" />
+
           <Widget name="Dust" dataKey="dust" />
+
           <Widget name="Humidity" dataKey="humidity" />
+
         </div>
-        <div style={{ textAlign: 'center', margin: '2rem' }}>
-</div>
 
         <Footer />
+
       </div>
+
     </QueryClientProvider>
+
   );
+
 }
